b11c02fb631c2ece56ceba4ca8d23a8ee98a5ecb
Loïca <95331041+NiceGuyMe@users.noreply.github.com>
2023-04-20 18:18:59 +0300

    chore: code refactor

--- src/Components/Videos.tsx
+++ src/Components/Videos.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard } from "./VideoCard";
-import { ChannelCard } from "./ChannelCard";
 import { Item } from "../Types/AllTypes";
 
 interface VideoProps {
@@ -11,11 +10,12 @@ interface VideoProps {
 export const Videos: React.FC<VideoProps> = (props: VideoProps) => {
   return (
     <Stack direction={"row"} flexWrap={"wrap"} justifyContent={"start"} gap={2}>
-      {props.videos.map((item: Item, idx: number) => {
+      {props.videos.map((item: Item) => {
         return (
-          <Box key={idx}>{item.id.videoId && <VideoCard video={item} />}</Box>
+            <Box key={item.id.videoId}>{item.id.videoId && <VideoCard video={item} />}</Box>
         );
       })}
     </Stack>
   );
 };
+
